eba0c81c084ed9e9403acb7ca2782f13274c5f8f
jmylk <13806561334@163.com>
2026-06-16 21:40:08 +0800

    Init

---
+++ scripts/main.js
@@ -0,0 +1,48 @@
+const ListItems = document.querySelectorAll("li");
+
+function toggleDone(e) {
+  if (!e.target.className) {
+    e.target.className = "done";
+  } else {
+    e.target.className = "";
+  }
+}
+
+ListItems.forEach((item) => {
+  item.addEventListener("click", toggleDone);
+});
+
+const myImage = document.querySelector("img");
+
+myImage.addEventListener("click", () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/立绘_林_1.png") {
+    myImage.setAttribute("src", "images/立绘_林_2.png");
+  } else {
+    myImage.setAttribute("src", "images/立绘_林_1.png");
+  }
+});
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `This lk's waifu Lin, ${myName},you son of bitch.`;
+  }
+}
+
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = `This lk's waifu Lin, ${storedName},you son of bitch.`;
+}
+
+myButton.addEventListener("click", () => {
+  setUserName();
+});
